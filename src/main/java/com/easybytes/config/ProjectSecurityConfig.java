73f3ff83075870097a70d1c65105cce1410771b4
coiacob <coiacob@pentalog.com>
2023-08-21 18:26:10 +0300

    created db and connected to it; added mysql in pom; added scripts; commented InMemoryUserDetailsManager and added JdbcUserDetailsManager; created customer entity and added repository

--- src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -3,16 +3,17 @@ package com.easybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration // during the startup spring will scan for all the beans that we gave define din this class
+@Configuration // during the startup spring will scan for all the beans that we have defined in this class
 public class ProjectSecurityConfig {
 
     @Bean
@@ -25,41 +26,47 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        /**
-         * Approach 1 where we use withDefaultPasswordEncoder() method
-         * while creating the user details
-         */
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        /**
+//         * Approach 1 where we use withDefaultPasswordEncoder() method
+//         * while creating the user details
+//         */
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("read")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(admin, user);
+//        /**
+//         * Approach 2 where we use NoOpPasswordEncoder Bean
+//         * while creating the user details
+//         */
+//        UserDetails admin = User.withUsername("admin")
 //                .password("12345")
 //                .authorities("admin")
 //                .build();
 //
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
+//        UserDetails user = User.withUsername("user")
 //                .password("12345")
 //                .authorities("read")
 //                .build();
 //
 //        return new InMemoryUserDetailsManager(admin, user);
-        /**
-         * Approach 2 where we use NoOpPasswordEncoder Bean
-         * while creating the user details
-         */
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
+//    }
 
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
