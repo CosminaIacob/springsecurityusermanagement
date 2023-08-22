6c21363a0b6058c1e7a59b178934615674595510
coiacob <coiacob@pentalog.com>
2023-08-22 15:15:16 +0300

    [Spring Security 6 Zero to Master along with JWT, OAUTH2 Udemy] Section 3: 35
    - added register endpoint
    - todo: refactor -> I don't like how to code was written in the tutorial

---
+++ src/main/java/com/easybytes/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.easybytes.controller;
+
+import com.easybytes.model.Customer;
+import com.easybytes.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final CustomerRepository customerRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity response = null;
+
+        try {
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity.status(HttpStatus.CREATED)
+                        .body("Given use details are successfully registered");
+            }
+        } catch (Exception e) {
+            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred due to " + e.getMessage());
+        }
+        return response;
+    }
+}
