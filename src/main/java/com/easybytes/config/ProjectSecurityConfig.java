df747b3b6d8919b08d4bdbc6b7894bd4b1a01a58
coiacob <coiacob@pentalog.com>
2023-08-23 13:40:04 +0300

    [Spring Security 6 Zero to Master along with JWT, OAUTH2 Udemy] Section 4: 44
    - PasswordEncoder with Bcrypt

--- src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -3,7 +3,7 @@ package com.easybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -65,9 +65,18 @@ public class ProjectSecurityConfig {
 //    }
 
 
+    /**
+     * All passwords will be treated as plain text by the spring security framework
+     *
+     * @return
+     */
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
